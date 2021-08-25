a379cd6d8e379eb596bec656eb48c2606d5be493
Jonathan Schilling <jonathan.schilling@ipp.mpg.de>
2021-08-25 16:00:01 +0200

    demo: plot evaluation positions for 2d integral over unit disk

---
+++ src/main/java/examples/PlotEvalPositions.java
@@ -0,0 +1,71 @@
+package examples;
+
+import aliceinnets.python.jyplot.JyPlot;
+import de.labathome.Cubature;
+import de.labathome.CubatureError;
+
+public class PlotEvalPositions {
+
+	public static double[][] nDimUnitSphere(double[][] x, Object fdata) {
+		int nDim = x.length;
+		int nPoints = x[0].length;
+		double[][] fval = new double[1][nPoints];
+
+		System.out.println("eval at "+nPoints+" pos");
+
+		JyPlot plt = (JyPlot) fdata;
+
+		double radius;
+		for (int i=0; i<nPoints; ++i) {
+
+			if (nPoints < 2000) {
+				plt.plot(x[0], x[1], "k.");
+			}
+
+			radius = 0.0;
+			for (int dim=0; dim<nDim; ++dim) {
+				radius += x[dim][i] * x[dim][i];
+			}
+			radius = Math.sqrt(radius);
+
+			if (radius < 1.0) {
+				fval[0][i] += 1.0;
+			}
+
+		}
+
+		return fval;
+	}
+
+	public static void plotEvalPositions() {
+		double[] xmin = { -1.1, -1.1 };
+		double[] xmax = {  1.1,  1.1 };
+
+		JyPlot plt = new JyPlot();
+		plt.figure();
+
+		plt.write("circle1 = plt.Circle((0, 0), 1.0, color='r')");
+		plt.write("fig = plt.gcf()");
+		plt.write("ax = fig.gca()");
+		plt.write("ax.add_patch(circle1)");
+
+		double[][] val_err = Cubature.integrate(PlotEvalPositions.class, "nDimUnitSphere",
+				xmin, xmax,
+				0.01, 0.0, CubatureError.INDIVIDUAL,
+				0,
+				plt);
+
+		double val = val_err[0][0];
+		System.out.println("unit disk area: " +  val);
+
+		plt.axis("equal");
+
+		plt.show();
+		plt.exec();
+	}
+
+	public static void main(String[] args) {
+		plotEvalPositions();
+	}
+
+}
